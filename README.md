816bab37fd553f05b5ac2b788f7b8c0fdd35a074
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2023-07-18 17:09:35 +0300

    intial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # eksctl-create-cluster
 scripts to quicky setup a K8s cluster using eksctl
+
+## How to use
+
+### Export AWS and EKS varibles
+
+```
+export AWS_PROFILE=asdfasdf
+export AWS_REGION=eu-west-1
+
+export EKS_CLUSTER_NAME=test-cluster-2
+```
+
+### Tweak eksctl_template.yaml
+
+You might want to edit the managed node groups or add extra addons
+
+### Generate the eksctl config file
+
+Substitute the env vars from the template file using this command:
+
+```
+envsubst < eksctl_template.yaml > eksctl_final.yaml
+```
+
+
+### Create the cluster
+
+Generate the cluster using the generated config file.
+This will take ~10 minutes, you need to wait.
+
+```
+eksctl create cluster -f eksctl_final.yaml
+```
+
+### Create the disk (EBS) storage class
+
+Create the storage class:
+
+```
+kubectl create -f k8s/sc_disk.yaml
+
+```
+
+And test:
+
+```
+kubectl create -f k8s/test_ebs.yaml
+kubectl get pvc
+```
+
+
+### Configure EFS storage
+
+This script will create an EFS file system (with a security group and a mount target).
+Next, it will install nfs-subdir-external-provisioner (via helm) and configure it to use the EFS.
+
+```
+./configure.sh
+```
+
+### Finally, test EFS is working
+```
+kubectl create -f k8s/test_efs.yaml
+kubectl get pvc
+```
---
+++ configure_efs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+
+#create efs file system
+export FILE_SYSTEM_ID=$(aws efs create-file-system | jq --raw-output '.FileSystemId')
+
+# Check the LifeCycleState of the file system using the following command and wait until it changes from creating to available before you proceed to the next step.
+aws efs describe-file-systems --file-system-id $FILE_SYSTEM_ID
+
+# extract the DNS name of the EFS
+export EFS_DNS_NAME=$FILE_SYSTEM_ID.efs.$AWS_REGION.amazonaws.com
+
+export VPC_ID=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --query "cluster.resourcesVpcConfig.vpcId" --output text)
+export CIDR_BLOCK=$(aws ec2 describe-vpcs --vpc-ids $VPC_ID --query "Vpcs[].CidrBlock" --output text)
+
+
+
+# create a security group for the EFS mount targets
+MOUNT_TARGET_GROUP_NAME="eks-efs-group-${EKS_CLUSTER_NAME}"
+MOUNT_TARGET_GROUP_DESC="NFS access to EFS from EKS worker nodes"
+MOUNT_TARGET_GROUP_ID=$(aws ec2 create-security-group --group-name $MOUNT_TARGET_GROUP_NAME --description "$MOUNT_TARGET_GROUP_DESC" --vpc-id $VPC_ID | jq --raw-output '.GroupId')
+aws ec2 authorize-security-group-ingress --group-id $MOUNT_TARGET_GROUP_ID --protocol tcp --port 2049 --cidr $CIDR_BLOCK
+
+
+# The following set of commands identifies the public subnets in your cluster VPC and creates a mount target in each one of them 
+# as well as associate that mount target with the security group you created above.
+
+TAG1=tag:alpha.eksctl.io/cluster-name
+TAG2=tag:kubernetes.io/role/elb
+subnets=($(aws ec2 describe-subnets --filters "Name=$TAG1,Values=$EKS_CLUSTER_NAME" "Name=$TAG2,Values=1" | jq --raw-output '.Subnets[].SubnetId'))
+for subnet in ${subnets[@]}
+do
+    echo "creating mount target in " $subnet
+    aws efs create-mount-target --file-system-id $FILE_SYSTEM_ID --subnet-id $subnet --security-groups $MOUNT_TARGET_GROUP_ID --no-cli-pager
+done
+
+# wait until all mount targets are available
+# if on Mac, install watch with:
+# brew install watch 
+watch "aws efs describe-mount-targets --file-system-id $FILE_SYSTEM_ID | jq --raw-output '.MountTargets[].LifeCycleState'"
+
+
+
+# add helm repo
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
+# install helm chart
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=$EFS_DNS_NAME \
+    --set nfs.path=/ \
+    --set storageClass.name=bns-network-sc
